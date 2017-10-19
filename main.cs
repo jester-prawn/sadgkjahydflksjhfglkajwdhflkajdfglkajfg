b35192fffaf44e247d1760d149ea357ccbde7ce7
jester-prawn <32911988+jester-prawn@users.noreply.github.com>
2017-10-19 16:59:42 -0400

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,9 @@
+//Main class that you run when you want to start a game of command-line Shonen Showdown
+//Handles most of the out-of-game menuing & initializes games
+
+
+Console.WriteLine("Welcome to Shonen Showdown./n
+  1: Vs AI/n
+  2: Local Multiplayer/n
+  3: Online/n");
+  
